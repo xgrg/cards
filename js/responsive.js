626ac19e2eaac04df195aac2ad81276a096434e1
grg <goperto@gmail.com>
2017-12-08 12:59:29 +0100

    fullscreen, preload, and reset_bodybox

--- js/responsive.js
+++ js/responsive.js
@@ -4,19 +4,38 @@ function adjustimg(){
                             //'border': '1px solid white'})
 }
 
-function preload(){
-  var arrayOfImages = []; //, 'media/bg.mp3']
-  $(arrayOfImages).each(function(i){   // Note the argument
-    var image = document.createElement('img');
-    console.log( arrayOfImages[i]);
-    image.setAttribute('src', arrayOfImages[i]);
-  });
-  var arrayOfImages = ['audio/bg.mp3']
-  $(arrayOfImages).each(function(i){   // Note the argument
-    console.log( arrayOfImages[i]);
-    var image = document.createElement('source');
-    image.setAttribute('src', arrayOfImages[i]);
-  });
+
+function reset_bodybox(){
+   html ='<div id="imagebox"></div>'+
+         '<div id="textbox">'+
+         '<div id="dialogbox"></div>'+
+         '<div id="choicebox"></div>'+
+         '</div>'
+   $('#bodybox').html(html)
+   config_bodybox();
+
+   if (fullscreen == 1)
+    screenfull.request($('#bodybox')[0]);
+   console.log("reset bodybox")
+}
+
+function preload(callback){
+    html = 'loading...'
+    $('#bodybox').html(html);
+    var array = []; //, 'media/bg.mp3']
+    $(array).each(function(i){   // Note the argument
+        html = html + array[i] + ' ';
+        console.log(array[i]);
+        if (array[i].indexOf('.mp3')!==-1)
+          var res = document.createElement('source');
+        else
+          var res = document.createElement('img');
+        res.setAttribute('src', array[i]);
+        $('#bodybox').html(html);
+    });
+    console.log('preload done')
+    reset_bodybox();
+    callback();
 }
 
 function loadScript(url, callback)
@@ -30,46 +49,42 @@ function loadScript(url, callback)
     head.appendChild(script);
 }
 
-h = window.innerHeight
-w = window.innerWidth
-if (w<h){ orientation = 'vertical'; }
-else { orientation = 'horizontal' }
-if (orientation == 'horizontal'){
-  h = h - 150;
-  w = h/1.5;
-}
-else {
-  w = w - 10;
-  h = w*1.4;
-}
+function config_bodybox(){
+    console.log('config bodybox')
+    h = window.innerHeight
+    w = window.innerWidth
+    if (w<h){ orientation = 'vertical'; }
+    else { orientation = 'horizontal' }
+    if (orientation == 'horizontal'){
+      h = h - 150;
+      w = h/1.5;
+    }
+    else {
+      w = w - 10;
+      h = w*1.4;
+    }
 
-var img = document.getElementById('imagebox').firstChild;
-$('#request').click(function () {
-  screenfull.request($('#bodybox')[0]);
-});
+    $("#orientation").text(orientation + ' ' + w + ' / ' + h)
 
-
-
-$("#orientation").text(orientation + ' ' + w + ' / ' + h)
-
-if (orientation == 'horizontal'){
-  $('#bodybox').css({'width': w})
-  $('body').css({'font-size':'20px',
-                 'width':w})
-  $('#textbox').css({'padding': '20px',
-                     'height': h/2,
-                     'width': w*.94})
-  $('#imagebox').css({'width': w*.98,
-                      'height': h/2})
-}
-else if (orientation == 'vertical' || orientation == '0'){
-  $('#bodybox').css({'width': w-15,
-                  'margin':'0px'})
-  $('body').css({'font-size':'11px',
-                 'width':w-10})
-  $('#imagebox').css({'width': w-14,
-                      'height': h/2})
-  $('#textbox').css({'padding': '10px',
-                     'height': h/2,
-                     'width': w-34})
+    if (orientation == 'horizontal'){
+        $('#bodybox').css({'width': w})
+        $('body').css({'font-size':'20px',
+                       'width':w})
+        $('#textbox').css({'padding': '20px',
+                           'height': h/2,
+                           'width': w*.94})
+        $('#imagebox').css({'width': w*.98,
+                            'height': h/2})
+    }
+    else if (orientation == 'vertical' || orientation == '0'){
+        $('#bodybox').css({'width': w-15,
+                        'margin':'0px'})
+        $('body').css({'font-size':'11px',
+                       'width':w-10})
+        $('#imagebox').css({'width': w-14,
+                            'height': h/2})
+        $('#textbox').css({'padding': '10px',
+                           'height': h/2,
+                           'width': w-34})
+    }
 }
